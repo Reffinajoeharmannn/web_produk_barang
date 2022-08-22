5dfe736f63b95f38f4d9176e2c55b45a68ca2e2d
Reffinajoeharmannn <reffinajoeharman27@gmail.com>
2022-08-22 12:52:47 +0700

    add register fitur

---
+++ application/models/M_user.php
@@ -0,0 +1,20 @@
+<?php
+
+class M_user extends CI_Model
+{
+	public function insert_user($id_user, $email, $password, $id_user_level)
+	
+	{
+		$this->db->trans_start();
+
+		$this->db->query("INSERT INTO user(id_user, username, email, password, id_user_level) VALUES 
+		('$id_user', '$username', '$email', '$password', '$id_user_level')");
+
+		$this->db->trans_complete();
+		if ($this->db->trans_status() == true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
